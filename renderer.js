5e5385a85e64197507abdf974eb572215072ef76
Helldub <nawakgaming.ng@gmail.com>
2023-09-27 20:12:58 +0100

    Helldubshit

--- renderer.js
+++ renderer.js
@@ -4,14 +4,17 @@ const scriptInput = document.getElementById('scriptInput');
 const userInput = document.getElementById('userInput');
 const runButton = document.getElementById('runButton');
 const output = document.getElementById('output');
-
+const cont = document.getElementById('Container');
+const submit = document.getElementById('Submit');
 runButton.addEventListener('click', () => {
   const filePath = scriptInput.files[0].path;
   ipcRenderer.send('run-script', filePath);
 });
 
 ipcRenderer.on('script-output', (event, data) => {
+  cont.style.visibility='visible';
   output.textContent += data;
+
 });
 
 ipcRenderer.on('script-exit', (event, code) => {
@@ -19,10 +22,12 @@ ipcRenderer.on('script-exit', (event, code) => {
 });
 
 // Listen for user input and send it to the main process
-userInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+Submit.addEventListener('click', (event) => {
+  console.log('hoooo')
     const input = userInput.value;
     userInput.value = ''; // Clear the input field
     ipcRenderer.send('user-input', input);
-  }
+
+
+
 });
